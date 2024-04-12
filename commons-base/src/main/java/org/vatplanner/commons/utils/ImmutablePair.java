7c5969c11c22e0facf06b3ad3ad85f1d4163a878
Daniel Neugebauer <dneuge@energiequant.de>
2024-04-12 21:46:44 +0200

    added ImmutablePair

---
+++ commons-base/src/main/java/org/vatplanner/commons/utils/ImmutablePair.java
@@ -0,0 +1,37 @@
+package org.vatplanner.commons.utils;
+
+import java.util.Objects;
+
+public class ImmutablePair<L, R> {
+    private final L left;
+    private final R right;
+
+    public ImmutablePair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public L getLeftValue() {
+        return left;
+    }
+
+    public R getRightValue() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ImmutablePair)) {
+            return false;
+        }
+
+        ImmutablePair<?, ?> other = (ImmutablePair<?, ?>) obj;
+
+        return Objects.equals(this.left, other.left) && Objects.equals(this.right, other.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+}
