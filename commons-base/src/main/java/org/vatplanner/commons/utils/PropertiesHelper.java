bc37914dbf5f6dd93f43bb332a1fa1562513cd64
Daniel Neugebauer <dneuge@energiequant.de>
2024-04-16 15:10:47 +0200

    PropertiesHelper: filterByKeys, extract

--- commons-base/src/main/java/org/vatplanner/commons/utils/PropertiesHelper.java
+++ commons-base/src/main/java/org/vatplanner/commons/utils/PropertiesHelper.java
@@ -1,7 +1,9 @@
 package org.vatplanner.commons.utils;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.function.Predicate;
 
 /**
  * Common helper methods to work with {@link Properties}.
@@ -25,5 +27,66 @@ public class PropertiesHelper {
             return Optional.empty();
         }
         return Optional.of(value);
+    }
+
+    /**
+     * Extracts all entries whose keys match the given {@link Predicate} to a new set of {@link Properties}.
+     *
+     * @param properties {@link Properties} to filter on
+     * @param predicate  condition to filter keys on
+     * @return new {@link Properties} holding all matched entries
+     */
+    public static Properties filterByKeys(Properties properties, Predicate<String> predicate) {
+        Properties out = new Properties();
+
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            String key = (String) entry.getKey();
+            if (predicate.test(key)) {
+                out.put(key, entry.getValue());
+            }
+        }
+
+        return out;
+    }
+
+    /**
+     * Extracts all entries whose keys start with the given prefix to a new set of {@link Properties}, removing the
+     * prefix.
+     * <p>
+     * Extracted sub-keys need to be collision-free and must not be empty.
+     * </p>
+     *
+     * @param properties {@link Properties} to extract entries from
+     * @param keyPrefix  key prefix to match and remove
+     * @return new {@link Properties} holding all entries that started with the wanted prefix; key prefix is removed
+     */
+    public static Properties extract(Properties properties, String keyPrefix) {
+        Properties out = new Properties();
+        int prefixLength = keyPrefix.length();
+
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            String key = (String) entry.getKey();
+            if (!key.startsWith(keyPrefix)) {
+                continue;
+            }
+
+            String subKey = key.substring(prefixLength);
+            if (subKey.isEmpty()) {
+                throw new IllegalArgumentException("Sub-key is empty after prefix removal: key=\"" + key + "\", prefix=\"" + keyPrefix + "\"");
+            }
+
+            Object previous = out.put(subKey, entry.getValue());
+            if (previous != null) {
+                throw new IllegalArgumentException(
+                    "Duplicate entry after prefix removal: key=\"" + key
+                        + "\", prefix=\"" + keyPrefix
+                        + "\", values=[" + previous
+                        + ", " + entry.getValue()
+                        + "]"
+                );
+            }
+        }
+
+        return out;
     }
 }
